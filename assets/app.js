3e9dd74443bbc7dfa0fe8b18b80fd8e84fa2f805
William Lu <32563160+williaml12@users.noreply.github.com>
2026-09-16 23:22:00 -0400

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+// Initialize UI
+const ui = new WebUI();
+ui.on_connect(onUIConnected);
+
+// Called when the websocket connection is established.
+function onUIConnected() {
+  // Write your logic here
+}
